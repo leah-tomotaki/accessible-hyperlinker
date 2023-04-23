19070350af90fec001e6e100af60ef8b80b667cb
leah-tomotaki <leah.tomotaki@tamu.edu>
2023-04-23 11:49:08 -0500

    Added background and content script changes

--- src/background.js
+++ src/background.js
@@ -19,3 +19,48 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+import getTitleAtUrl from 'get-title-at-url';
+import DomParser  from 'dom-parser';
+// const { JSDOM } = jsdom;
+
+const get_title_at_url = async (url) => {
+
+  try {
+    const response = await fetch(url);
+    const html_text = await response.text()
+	  const parser = new DomParser();
+    const htmlDoc = parser.parseFromString(html_text, 'text/html');
+    let title = htmlDoc.getElementsByTagName('title')[0];
+
+    title = title.innerHTML;
+    console.log(title);
+    return title;
+  } catch (error) {
+
+    return url;
+  }
+}
+
+const get_titles = async (links) => {
+  let titles = [];
+  for(var i = 0; i < links.length; i++) {
+    let title = await get_title_at_url(links[i]);
+    titles.push(title);
+  }
+  return titles;
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'LINKS') {
+    get_titles(request.payload.links).then(titles => sendResponse({
+      titles,
+    }));
+    // Log message coming from the `request` parameter
+    // console.log(titles);
+    // Send a response message
+    
+  }
+  return true;
+});
